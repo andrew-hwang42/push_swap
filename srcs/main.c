3ecfa3ba3abda20078d6007b97b5c5dd5c5ee7b8
andrew-hwang42 <mh3052611@gmail.com>
2025-05-30 03:22:03 +0200

    20250530_03:22

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ahwang <ahwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 03:55:34 by ahwang            #+#    #+#             */
-/*   Updated: 2025/05/29 22:01:31 by ahwang           ###   ########.fr       */
+/*   Updated: 2025/05/30 03:20:55 by ahwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int	main(int argc, char **argv)
 	t_stack	*a;
 	t_stack	*b;
 
-	if (argc < 2)
-		std_err();
+	if (argc == 2)
+		return (0);
 	argv++;
 	if (!check_valid_input(argv))
 		std_err();
